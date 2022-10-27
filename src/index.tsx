e43fd96a921c794cb50961f3a7138d1784728b5c
Nadi Duno <nadiduno@gmail.com>
2022-10-26 22:53:27 -0300

    react

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//import { render } from 'react-dom';
+import { App } from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render( <App /> )
+// render (<App />,document.getElementById('root')) 
